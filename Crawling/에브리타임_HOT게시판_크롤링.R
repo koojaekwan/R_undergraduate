ccc7b42ea7e43b5e890e7d4a7e4205011a398a40
JaeKwan <ku1843@naver.com>
2020-07-25 04:15:47 +0900

    셀레니움 연습

--- Crawling/에브리타임_HOT게시판_크롤링.R
+++ Crawling/에브리타임_HOT게시판_크롤링.R
@@ -1,3 +1,7 @@
+# cd C:\Rselenium
+# java -Dwebdriver.gecko.driver="geckodriver.exe" -jar selenium-server-standalone-3.141.59.jar -port 4445
+
+
 library(RSelenium)
 library(seleniumPipes)
 library(rvest)
